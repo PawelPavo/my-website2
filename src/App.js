f110d2d765593b1070e6666ac6984839b71ef255
PawelPavo <Pawelebllc@gmail.com>
2020-12-06 20:09:31 -0500

    fixed navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Navigation from './components/Navigation';
 import { GlobalStyles } from './utils/globalStyles'
 import { lightTheme, darkTheme } from './utils/themes';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import NavigationDesktop from './components/NavigationDesktop';
+
 import Home from './Views/Home';
 import Projects from './Views/Projects';
 import Contact from './Views/Contact';
@@ -17,12 +19,23 @@ function App() {
     setChecked(checked)
   }
 
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 1024;
+
+  React.useEffect(() => {
+      const handleWindowResize = () => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleWindowResize);
+
+      return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
   return (
     <>
       <BrowserRouter>
         <ThemeProvider theme={!checked ? lightTheme : darkTheme} >
           <GlobalStyles />
-          <Navigation checked={checked} handleTheme={handleTheme} />
+          {width < breakpoint ? <Navigation checked={checked} handleTheme={handleTheme} /> : <NavigationDesktop checked={checked} handleTheme={handleTheme}/>}
+          {/* <Navigation checked={checked} handleTheme={handleTheme} /> */}
           <Switch>
             <Route exact path="/">
             </Route>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,49 +7,49 @@ import Burger from '../components/burger/Burger'
 function Navigation({ checked, handleTheme }) {
     const [open, setOpen] = React.useState(false);
 
-    return (
-        <>
-            <GoogleFontLoader
-                fonts={[
-                    {
-                        font: 'Ubuntu',
-                        weights: [400, '400i'],
-                    },
-                ]}
-                subsets={['cyrillic-ext', 'greek']}
-            />
-            <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-4 ${!checked ? 'bg-white' : 'bg-primary'}`}>
-                {/* <NavLink className="navbar-brand" to="/">
+        return (
+            <>
+                <GoogleFontLoader
+                    fonts={[
+                        {
+                            font: 'Ubuntu',
+                            weights: [400, '400i'],
+                        },
+                    ]}
+                    subsets={['cyrillic-ext', 'greek']}
+                />
+                <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-4 ${!checked ? 'bg-white' : 'bg-primary'}`}>
+                    {/* <NavLink className="navbar-brand" to="/">
                     <img className=""
                         src={!checked ? `${"https://news-api.s3.us-east-2.amazonaws.com/MeEyesOpen2.png"}` : `${"https://news-api.s3.us-east-2.amazonaws.com/MeEyesClosed2.png"}`}
                         alt="Pawel_Jaskolski" style={{ height: "65px", width: "auto" }} />
                 </NavLink> */}
-                <div className="">
-                <ToggleSwitch checked={checked} handleTheme={handleTheme} className="" />
-                    <Burger className="" open={open} setOpen={setOpen} checked={checked} />
-                </div>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav" style={{ fontFamily: "Ubuntu" }}>
-                        {/* <li className="nav-item ">
+                    <div className="">
+                        <ToggleSwitch checked={checked} handleTheme={handleTheme} id="test" />
+                        <Burger className="" open={open} setOpen={setOpen} checked={checked} />
+                    </div>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav navbar-collapse" style={{ fontFamily: "Ubuntu" }}>
+                            {/* <li className="nav-item ">
                             <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/">Home</NavLink>
                         </li> */}
-                        <li className="nav-item">
-                            <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/home">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/projects">Projects</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/about">About Me</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/contact">Contact</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    );
-}
+                            <li className="nav-item" type="button" data-toggle="collapse" data-target="#navbarNav">
+                                <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/home">Home</NavLink>
+                            </li>
+                            <li className="nav-item" type="button" data-toggle="collapse" data-target="#navbarNav">
+                                <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/projects">Projects</NavLink>
+                            </li>
+                            <li className="nav-item" type="button" data-toggle="collapse" data-target="#navbarNav">
+                                <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/about">About Me</NavLink>
+                            </li>
+                            <li className="nav-item" type="button" data-toggle="collapse" data-target="#navbarNav">
+                                <NavLink className={`nav-link nav-text mx-5 ${!checked ? '' : 'text-light'}`} to="/contact">Contact</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </>
+        );
+    }
 
-export default Navigation;
+    export default Navigation;
